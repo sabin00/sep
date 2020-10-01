a3d0a0ad8a7354a9eb8543af91b6d8dde872d1a9
sabin00 <ozzyo3839@gmail.com>
2020-10-01 13:40:42 +0545

    First push

---
+++ s.php
@@ -0,0 +1,31 @@
+<?php 
+$serverName = 'localhost';
+$userName = 'root';
+$password = '';
+$dbName = 'sept_php';
+
+
+$conn = new mysqli($serverName, $userName, $password, $dbName);
+
+
+if($conn->connect_error){
+	die ('Failed:' . $conn->connect_error);
+
+}
+
+echo " connection successful";
+
+
+
+$sql = "select * from users"; //sql query to fetch all data
+
+$result = $conn->query($sql); //execute the given data
+
+
+echo "<pre>";
+print_r($result); //debugging
+
+$conn ->close();
+
+
+?>
